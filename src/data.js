497c883a1315ca315bc4fd050030d015dd312586
kavyabhat99 <kavyabhat98@gmail.com>
2021-09-24 17:28:13 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,46 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'CCTv',
+        price: 3000,
+        image: 'https://t4.ftcdn.net/jpg/02/31/31/17/240_F_231311708_cu4Gk5j80vcvLW6h61RWl58o7rEN2AgU.jpg',
+      },
+      {
+        id: '2',
+        name: 'Headphones',
+        price: 2400,
+        image: 'https://t4.ftcdn.net/jpg/03/33/83/73/240_F_333837337_nP8kqHbQ28y30g76WDhLyhInsQi4Ezus.jpg',
+      },
+      {
+        id: '3',
+        name: 'SmartWatch',
+        price: 1000,
+        image: 'https://as2.ftcdn.net/v2/jpg/00/80/60/03/1000_F_80600305_U4VHqnbvOPKSzpfJbNnp2mj1cjBYHmZc.jpg',
+      },
+
+      {
+        id: '4',
+        name: 'Speaker',
+        price: 2000,
+        image: 'https://t3.ftcdn.net/jpg/01/74/69/06/240_F_174690649_wPsO9QZPCIYbOReYiFbT1NVJYcUia0QN.jpg',
+      },
+
+      {
+        id: '5',
+        name: 'MicroChip',
+        price: 1000,
+        image: 'https://as2.ftcdn.net/v2/jpg/03/80/07/17/1000_F_380071776_x6jpEOa6sMFHBACMtATSbrNeiRLd4Mar.jpg',
+      },
+
+      {
+        id: '6',
+        name: 'Powerbank',
+        price: 700,
+        image: 'https://t3.ftcdn.net/jpg/02/84/15/30/240_F_284153027_RpZF23osht2QlrOGFnuIwMKyZXnuW0xn.jpg',
+      },
+
+      
+    ],
+  };
+  export default data;
